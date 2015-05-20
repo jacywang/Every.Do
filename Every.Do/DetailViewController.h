5bb895d6c29472cba29bc8f034c8e38b11ce401c
Jacy Wang <jacywj@gmail.com>
2015-05-20 11:12:41 -0700

    Initialize repo

---
+++ Every.Do/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Every.Do
+//
+//  Created by JIAN WANG on 5/20/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Every.Do/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  Every.Do
+//
+//  Created by JIAN WANG on 5/20/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
